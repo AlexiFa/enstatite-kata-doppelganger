a6747d6fd1175f164b21eb0e7a86ded4e2deb7d3
Alexis Faure <113013323+AlexiFa@users.noreply.github.com>
2025-01-08 14:44:07 +0100

    safe-calculator done

--- python/tests/test_safe_calculator.py
+++ python/tests/test_safe_calculator.py
@@ -1,7 +1,14 @@
 import pytest
-
+from safe_calculator import SafeCalculator
+from authorizer import Authorizer
 
 def test_divide_should_not_raise_any_error_when_authorized():
     # TODO: write a test that fails due to the bug in
     # SafeCalculator.add
-    pass
+
+    authorizer = Authorizer(True)
+    safeCalculator = SafeCalculator(authorizer)
+    try:
+        safeCalculator.add(1, 2) == 3
+    except Exception as e:
+        pytest.fail("Exception raised unexpectedly")
